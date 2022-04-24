ef8999e9cac3d44f18cc99347753014d3ccf6415
kheanglov <lovsokheang@gmail.com>
2022-04-25 03:18:37 +0700

    add pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ env('APP_NAME', 'Lara') }}</title>
-
+    @laravelPWA
     @stack('before_styles')
 
     <!-- Fonts -->
