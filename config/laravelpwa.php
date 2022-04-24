5b99f80bfa364bab6c650b7e7da9cd467c876cdc
kheanglov <lovsokheang@gmail.com>
2022-04-25 03:36:52 +0700

    update manifest

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -7,50 +7,19 @@ return [
         'short_name' => 'BROBug',
         'description' => 'News tech website',
         'start_url' => '/admin/login',
-        'background_color' => '#191d21',
-        'theme_color' => '#191d21',
+        'background_color' => '#fff',
+        'theme_color' => '#fff',
         'display' => 'standalone',
         'orientation' => 'any',
         'status_bar' => 'black',
         "lang" => "en-US",
         'icons' => [
-            '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
-            ],
-            '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
-            ],
-            '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
-            ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
-            ],
-            '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/assets/bugbro-144x144.png',
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/assets/bugbro-192x192.png',
             ],
-            '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
-            ],
-            '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
-                "purpose" => "maskable"
-            ],
-        ],
-        'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
         ],
         'custom' => []
     ]
